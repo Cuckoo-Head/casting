b8d5f93087b48dc46ecc6a38bc8620dc2c4415e3
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-12 19:19:30 -0300

    tentativas de casting adicionadas

--- src/com/dio/base/Main.java
+++ src/com/dio/base/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int a = 1000;
+		byte b = (byte)a;
+		System.out.println(b); //??
+	
+		double c = 21.564;
+		int d = (int) c;
+		System.out.println(d);
+		
+		/***String e = "S";
+		char f = (char) e;
+		System.out.println(f);*/ //can't cast from string to char
+		
+		long g = 83748457L;
+		short h = (short) g;
+		System.out.println(h); //??
 	}
 
 }
